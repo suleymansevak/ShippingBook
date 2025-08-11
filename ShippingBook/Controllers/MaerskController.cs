ee194fa17342ad7a0b414743f3898260c126dbd5
suleymansevak <suleyman.sevak@outlook.com>
2025-08-11 16:14:50 +0300

    first

---
+++ ShippingBook/Controllers/MaerskController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShippingBook.Core.Entities;
+using ShippingBook.Services.Abstract;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShippingBook.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MaerskController : Controller
+    {
+        private readonly IMaerskService _maerskService;
+        public MaerskController(IMaerskService maerskService)
+        {
+            _maerskService = maerskService;
+        }
+
+        [HttpPost("GetTable")]
+        public async Task<BaseResponse<MaerskResponse>> GetTable(MaerskRequest request) =>
+             await _maerskService.GetTable(request);
+    }
+}
+
